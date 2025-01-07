8185f07e6365670ddb56f474e8fc46ad62d1aa28
Toan Tran <thanhnoobs@gmail.com>
2025-01-08 06:48:04 +0700

    Update: Feedback, routing

--- src/store/useInfoStore.js
+++ src/store/useInfoStore.js
@@ -1,8 +1,15 @@
 import { create } from "zustand";
 import { getGraveRows } from "../services/graveRowService";
 
-export const useInfoStore = create((set) => ({
+export const useInfoStore = create((set, get) => ({
   grave_rows: [],
+  grave_rows_entries: [],
+  findGraveRowIdByName: (name) => {
+    let grave_rows = get().grave_rows_entries;
+    let entry = grave_rows.find((row) => row.rowName === name);
+    let grave_row_id = entry ? entry.id : null;
+    return grave_row_id;
+  },
 
   isLoadingInfo: false,
   loadInfo: async () => {
@@ -11,6 +18,7 @@ export const useInfoStore = create((set) => ({
     try {
       set({ isLoadingInfo: true });
       let grave_rows = await getGraveRows();
+      let grave_rows_entries = grave_rows;
       // Build from grave rows list
       if (grave_rows.length > 0) {
         let formatted_grave_rows = grave_rows.reduce((acc, row) => {
@@ -21,7 +29,11 @@ export const useInfoStore = create((set) => ({
           return acc;
         }, {});
 
-        set({ grave_rows: formatted_grave_rows, isLoading: false });
+        set({
+          grave_rows_entries: grave_rows_entries,
+          grave_rows: formatted_grave_rows,
+          isLoading: false,
+        });
       }
       set({ isLoadingInfo: false });
     } catch (error) {
